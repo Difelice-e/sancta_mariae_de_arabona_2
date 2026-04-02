91a1b4b299ea973950e43e6346aa0f66d5353528
Emanuele Di Felice <difelice.emanuele@gmail.com>
2026-04-02 14:59:39 +0200

    chore: remove obsolete FacilitaMap/StorytellingSection files and imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from '@/components/home/Hero'
-import StorytellingSection from '@/components/home/StorytellingSection'
 import Atmosphere from '@/components/home/Atmosphere'
 import Highlights from '@/components/home/Highlights'
 import CtaSection from '@/components/home/CtaSection'
@@ -8,7 +7,6 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <StorytellingSection />
       <Atmosphere />
       <Highlights />
       <CtaSection />
